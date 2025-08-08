e1687801df3ecea35dbfe3b5c95c3051868023e9
Alejandro Schmeil <alejandroschmeil@gmail.com>
2025-08-08 20:03:33 -0300

    other

--- app/views/gantt/gantt_controller.py
+++ app/views/gantt/gantt_controller.py
@@ -1,17 +1,6 @@
-from app.core.task.task_manager import load_tasks
-from app.views.gantt.gantt_data_service import construir_datos_gantt_por_proyecto
 import pandas as pd
-from app.core.init_data import generar_datos_iniciales
 from datetime import timedelta
-
-def obtener_datos_gantt(project_list):
-    datos = []
-    for project in project_list:
-        tasks = load_tasks(project)
-        gantt_data = construir_datos_gantt_por_proyecto(tasks, project)
-        if gantt_data:
-            datos.append(gantt_data)
-    return datos
+from app.core.init_data import generar_datos_iniciales
 
 def obtener_dataframe_proyectos(project_list):
     """
