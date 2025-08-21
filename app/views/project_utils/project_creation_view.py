ed342c9445f54ca84b5aca44686f3981fda7176a
Alejandro Schmeil <alejandroschmeil@gmail.com>
2025-08-20 22:40:00 -0300

    Inde

--- app/views/project_utils/project_creation_view.py
+++ app/views/project_utils/project_creation_view.py
@@ -4,26 +4,26 @@ import pandas as pd
 def render_project_form(datos):
     with st.expander("📌 Detalles Proyecto", expanded=True):
         cliente = st.text_input("👤 Cliente", value=datos.get("Cliente", ""), key="cliente")
-        responsable = st.text_input("👤 Responsable", value=datos.get("Responsable", ""), key="responsable")
-        estado = st.selectbox("Estado", ["Pendiente", "En progreso", "Finalizado"],
-                              index=["Pendiente", "En progreso", "Finalizado"].index(datos.get("Estado", "Pendiente")),
-                              key="estado")
         localidad = st.text_input("🌍 Localidad", value=datos.get("Localidad", ""), key="localidad")
         metros = st.number_input("📏 Metros²", min_value=0, value=datos.get("Metros²", 0), step=1, key="metros")
-        fecha_inicio_val = pd.to_datetime(datos.get("Inicio", str(pd.Timestamp.today().date()))).date()
-        fecha_inicio = st.date_input("📅 Fecha de inicio", value=fecha_inicio_val, key="fecha_inicio")
         duracion_valor = datos.get("Duración estimada (días)", 1)
         if duracion_valor < 1:
             duracion_valor = 1
         duracion = st.number_input("⏱️ Duración estimada (días)", min_value=1, value=duracion_valor, step=1, key="duracion")
+        responsable = st.text_input("🏭 Fábrica Responsable", value=datos.get("Responsable", ""), key="responsable")
+        estado = st.selectbox("Estado", ["Pendiente", "En progreso", "Finalizado"],
+                              index=["Pendiente", "En progreso", "Finalizado"].index(datos.get("Estado", "Pendiente")),
+                              key="estado")
+        fecha_inicio_val = pd.to_datetime(datos.get("Inicio", str(pd.Timestamp.today().date()))).date()
+        fecha_inicio = st.date_input("📅 Fecha de inicio", value=fecha_inicio_val, key="fecha_inicio")
         
     
     return {
         "Cliente": cliente,
-        "Responsable": responsable,
-        "Estado": estado,
         "Localidad": localidad,
         "Metros²": metros,
-        "Inicio": str(fecha_inicio),
         "Duración estimada (días)": duracion,
+        "Fábrica Responsable": responsable,
+        "Estado": estado,
+        "Inicio": str(fecha_inicio),
     }
