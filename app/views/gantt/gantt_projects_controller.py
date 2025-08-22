a5121c676295d69d65e8fbe1a25b1c1f9e2a76d1
Alejandro Schmeil <alejandroschmeil@gmail.com>
2025-08-21 21:53:23 -0300

    LIA

--- app/views/gantt/gantt_projects_controller.py
+++ app/views/gantt/gantt_projects_controller.py
@@ -7,7 +7,6 @@ from app.core.task.project_duration import calcular_duracion_proyecto
 import streamlit as st
 
 
-
 def precalcular_duraciones_proyectos(project_list):
     if "duracion_proyectos" not in st.session_state:
         st.session_state["duracion_proyectos"] = {}
@@ -26,43 +25,6 @@ def precalcular_duraciones_proyectos(project_list):
             st.session_state["duracion_proyectos"][project_name] = (inicio, fin, rango_dias)
 
 
-            
-'''
-def obtener_grafico_gantt_proyectos(project_list):
-    df = obtener_dataframe_proyectos(project_list)
-
-    if df.empty:
-        return None
-
-    fig = px.timeline(
-        df,
-        x_start="Inicio",
-        x_end="Fin",
-        y="Proyecto",
-        color="Estado",
-        hover_data={
-            "Responsable": True,
-            "Cliente": True,
-            "Localidad": True,
-            "Metros²": True,
-            "Duración estimada": True,
-            "Estado": True,
-            "Inicio": False,
-            "Fin": False,
-            "Proyecto": False,
-        }
-    )
-
-    fig.update_yaxes(autorange="reversed")
-    fig.update_layout(
-        height=600,
-        margin=dict(l=20, r=20, t=40, b=20),
-        xaxis_title="Fechas de ejecución"
-    )
-
-    return fig
-'''
-
 def obtener_grafico_gantt_proyectos(project_list):
     registros = []
 
@@ -70,12 +32,10 @@ def obtener_grafico_gantt_proyectos(project_list):
         # Tomar la duración previamente calculada
         duracion_info = st.session_state.get("duracion_proyectos", {}).get(project_name)
         if duracion_info is None:
-            # Si no existe, todavía no se cargó el proyecto
             continue
 
         inicio_total, fin_total, duracion_total = duracion_info
 
-        # Mostrar en consola
         print(f"Duración total del proyecto '{project_name}': {duracion_total} días "
               f"({inicio_total.date()} → {fin_total.date()})")
 
@@ -97,7 +57,11 @@ def obtener_grafico_gantt_proyectos(project_list):
         x_end="Fin",
         y="Proyecto",
         color="Proyecto",
-        hover_data={"Duración real": True, "Inicio": False, "Fin": False, "Proyecto": False}
+        hover_data={
+            "Duración real": True,
+            "Inicio": True,
+            "Fin": True
+        }
     )
 
     fig.update_yaxes(autorange="reversed")
