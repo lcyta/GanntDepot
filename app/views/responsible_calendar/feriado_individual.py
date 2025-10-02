121a7d58ae6519ae2e28d1e9d47f766d23f25f18
lcyta <lcyta@ffyb.uba.ar>
2025-10-02 19:05:52 +0000

    Dela25

--- app/views/responsible_calendar/feriado_individual.py
+++ app/views/responsible_calendar/feriado_individual.py
@@ -11,13 +11,15 @@ from app.core.calendar.calendar_updater import (
 
 def manejar_feriado_individual(nombre, feriados):
     with st.expander("➕ Agregar / ❌ Eliminar feriado individual"):
+        st.markdown("### ➕ Agregar fecha de feriado")
         accion_agregar = ui_agregar_feriado(nombre)
         if accion_agregar and accion_agregar[0] == "agregar":
             _, fecha, descripcion = accion_agregar
             agregar_feriado_responsable(nombre, fecha, descripcion)
             st.success("Feriado agregado.")
             st.rerun()
-
+        st.markdown("---")
+        st.markdown("### ❌ Eliminar fecha de Feriado")
         accion_eliminar = ui_eliminar_feriado(nombre, feriados)
         if accion_eliminar and accion_eliminar[0] == "eliminar":
             _, fecha = accion_eliminar
--- app/views/responsible_calendar/manejar_rango_feriados.py
+++ app/views/responsible_calendar/manejar_rango_feriados.py
@@ -3,6 +3,6 @@ from app.views.responsible_calendar.ui_agregar_rango import ui_agregar_rango
 from app.views.responsible_calendar.ui_eliminar_rango import ui_eliminar_rango
 
 def manejar_rango_feriados(nombre):
-    with st.expander("📆 Agregar / ❌ Eliminar rango de feriados"):
+    with st.expander("➕ Agregar / ❌ Eliminar rango de feriados"):
         ui_agregar_rango(nombre)
         ui_eliminar_rango(nombre)
